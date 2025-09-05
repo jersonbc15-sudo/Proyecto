be84ef9283438485e22a3254a1f30824db29c8da
Jotin <jersonbc15@gmail.com>
2025-09-05 09:24:32 -0500

    Create Proyecto.java

---
+++ Proyecto.java
@@ -0,0 +1,31 @@
+import javax.swing.JOptionPane;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int clave = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > clave) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = clave;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+
+        insertionSort(arr);
+
+        // Construir el mensaje con el array ordenado
+        StringBuilder resultado = new StringBuilder("Array ordenado:\n");
+        for (int num : arr) {
+            resultado.append(num).append(" ");
+        }
+
+        // Mostrar cuadro emergente con el resultado
+        JOptionPane.showMessageDialog(null, resultado.toString(), "Resultado", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
